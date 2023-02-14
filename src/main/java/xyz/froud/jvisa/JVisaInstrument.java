ae2fc0db1e8aee54c650d067406c6bc29235f5ec
Peter Froud <pfroud@gmail.com>
2023-02-13 23:38:52 -0800

    Add support for read/write terminators
    See https://pyvisa.readthedocs.io/en/latest/introduction/communication.html

--- src/main/java/xyz/froud/jvisa/JVisaInstrument.java
+++ src/main/java/xyz/froud/jvisa/JVisaInstrument.java
@@ -42,6 +42,14 @@ public class JVisaInstrument implements AutoCloseable {
     private final JVisaLibrary VISA_LIBRARY;
     public final String RESOURCE_NAME;
 
+    /**
+     * A string appended to the end of every string sent to the instrument. If it null then nothing is appended.
+     *
+     * @see <a href="https://pyvisa.readthedocs.io/en/latest/introduction/communication.html">PyVISA docs page
+     * "Communicating with your instrument"</a>
+     */
+    private String writeTerminator = null;
+
     public JVisaInstrument(JVisaResourceManager resourceManager, NativeLongByReference instrumentHandle, String resourceName) {
         RESOURCE_MANAGER = resourceManager;
         VISA_LIBRARY = resourceManager.VISA_LIBRARY;
@@ -100,7 +108,8 @@ public class JVisaInstrument implements AutoCloseable {
     }
 
     /**
-     * Sends a command to the instrument.
+     * Sends a command to the instrument. If setWriteTerminator() was called with a non-null string, the terminator will
+     * be appended to the string before sending it to the instrument.
      *
      * @param command the command to send
      *
@@ -108,8 +117,14 @@ public class JVisaInstrument implements AutoCloseable {
      * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/viwrite.html">viWrite</a>
      */
     public void write(String command) throws JVisaException {
-        final ByteBuffer buffer = JVisaUtils.stringToByteBuffer(command);
-        final int commandLength = command.length();
+        final String commandWithTerminator;
+        if (writeTerminator != null) {
+            commandWithTerminator = command + writeTerminator;
+        } else {
+            commandWithTerminator = command;
+        }
+        final ByteBuffer buffer = JVisaUtils.stringToByteBuffer(commandWithTerminator);
+        final int commandLength = commandWithTerminator.length();
 
         final NativeLongByReference returnCount = new NativeLongByReference();
         final NativeLong errorCode = VISA_LIBRARY.viWrite(INSTRUMENT_HANDLE,
@@ -213,35 +228,81 @@ public class JVisaInstrument implements AutoCloseable {
     /**
      * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vigetattribute.html">viGetAttribute</a>
      */
-    public String getAttribute(int attr) throws JVisaException {
-        final Memory mem = new Memory(256);
+    public String getAttributeString(int attr) throws JVisaException {
+        return getAttributeString(attr, DEFAULT_BUFFER_SIZE);
+    }
 
-        final NativeLong errorCode = VISA_LIBRARY.viGetAttribute(INSTRUMENT_HANDLE, new NativeLong(attr), mem);
+    /**
+     * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vigetattribute.html">viGetAttribute</a>
+     */
+    public String getAttributeString(int attr, int bufferSize) throws JVisaException {
+        /*
+        Calling memory.getString() calls the native method
+            com.sun.jna.Native.getStringBytes()
+        so I think it does something more interesting than getting bytes
+        from the memory then calling the String constructor on the bytes.
+         */
+        return getAttributeMemory(attr, bufferSize).getString(0);
+    }
+
+    /**
+     * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vigetattribute.html">viGetAttribute</a>
+     */
+    public byte[] getAttributeBytes(int attr) throws JVisaException {
+        return getAttributeBytes(attr, DEFAULT_BUFFER_SIZE);
+    }
+
+    /**
+     * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vigetattribute.html">viGetAttribute</a>
+     */
+    public byte[] getAttributeBytes(int attr, int bufferSize) throws JVisaException {
+        return getAttributeMemory(attr, bufferSize).getByteArray(0, bufferSize);
+    }
+
+    /**
+     * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vigetattribute.html">viGetAttribute</a>
+     */
+    private Memory getAttributeMemory(int attr, int bufferSize) throws JVisaException {
+        final Memory rv = new Memory(bufferSize);
+
+        final NativeLong errorCode = VISA_LIBRARY.viGetAttribute(INSTRUMENT_HANDLE, new NativeLong(attr), rv);
         RESOURCE_MANAGER.checkError(errorCode, "viGetAttribute");
 
         // apparently we can't dispose or free or finalize a Memory, just need to let JVM call finalize()
-        return mem.getString(0, "UTF-8");
+        return rv;
+    }
+
+    /**
+     * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/visetattribute.html">viSetAttribute</a>
+     */
+    public void setAttribute(int attr, long value)throws JVisaException{
+        final NativeLong status = VISA_LIBRARY.viSetAttribute(
+                INSTRUMENT_HANDLE,
+                new NativeLong(attr),
+                new NativeLong(value)
+        );
+        RESOURCE_MANAGER.checkError(status, "viSetAttribute");
     }
 
     /**
      * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_manf_name.html">VI_ATTR_MANF_NAME</a>
      */
     public String getManufacturerName() throws JVisaException {
-        return getAttribute(JVisaLibrary.VI_ATTR_MANF_NAME);
+        return getAttributeString(JVisaLibrary.VI_ATTR_MANF_NAME);
     }
 
     /**
      * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_model_name.html">VI_ATTR_MODEL_NAME</a>
      */
     public String getModelName() throws JVisaException {
-        return getAttribute(JVisaLibrary.VI_ATTR_MODEL_NAME);
+        return getAttributeString(JVisaLibrary.VI_ATTR_MODEL_NAME);
     }
 
     /**
      * @see <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_usb_serial_num.html">VI_ATTR_SERIAL_NUM</a>
      */
     public String getUsbSerialNumber() throws JVisaException {
-        return getAttribute(JVisaLibrary.VI_ATTR_USB_SERIAL_NUM);
+        return getAttributeString(JVisaLibrary.VI_ATTR_USB_SERIAL_NUM);
     }
 
     /**
@@ -305,6 +366,77 @@ public class JVisaInstrument implements AutoCloseable {
                 (short) JVisaLibrary.VI_ALL_MECH //mechanism
         );
         RESOURCE_MANAGER.checkError(status, "viDiscardEvents");
+    }
+
+    /**
+     * VI_ATTR_TERMCHAR is the termination character. When the termination character is read and VI_ATTR_TERMCHAR_EN is
+     * enabled during a read operation, the read operation terminates. The default is '\n' (line feed).
+     * <p>
+     * For a Serial INSTR session, VI_ATTR_TERMCHAR is Read/Write when the corresponding session is not enabled to
+     * receive VI_EVENT_ASRL_TERMCHAR events. When the session is enabled to receive VI_EVENT_ASRL_TERMCHAR events, the
+     * attribute VI_ATTR_TERMCHAR is Read Only. For all other session types, the attribute VI_ATTR_TERMCHAR is always
+     * Read/Write.
+     *
+     * @see
+     * <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_termchar.html">VI_ATTR_TERMCHAR</a>
+     * @see <a href="https://github.com/pyvisa/pyvisa/blob/1.13.0/pyvisa/resources/messagebased.py#L96">read_termination
+     * in PyVISA</a>
+     * @see <a href="https://pyvisa.readthedocs.io/en/latest/introduction/communication.html">PyVISA docs page
+     * "Communicating with your instrument"</a>
+     */
+    public void setReadTerminationCharacter(char readTerminator) throws JVisaException {
+        setAttribute(JVisaLibrary.VI_ATTR_TERMCHAR, (int) readTerminator);
+    }
+
+    /**
+     * VI_ATTR_TERMCHAR is the termination character. When the termination character is read and VI_ATTR_TERMCHAR_EN is
+     * enabled during a read operation, the read operation terminates. The default is '\n' (line feed).
+     * <p>
+     * For a Serial INSTR session, VI_ATTR_TERMCHAR is Read/Write when the corresponding session is not enabled to
+     * receive VI_EVENT_ASRL_TERMCHAR events. When the session is enabled to receive VI_EVENT_ASRL_TERMCHAR events, the
+     * attribute VI_ATTR_TERMCHAR is Read Only. For all other session types, the attribute VI_ATTR_TERMCHAR is always
+     * Read/Write.
+     *
+     * @see
+     * <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_termchar.html">VI_ATTR_TERMCHAR</a>
+     */
+    public char getReadTerminationCharacter() throws JVisaException {
+        return (char) getAttributeBytes(JVisaLibrary.VI_ATTR_TERMCHAR, 1)[0];
+    }
+
+    /**
+     * VI_ATTR_TERMCHAR_EN is a flag that determines whether the read operation should terminate when a termination
+     * character is received. The default is false. This attribute is ignored if VI_ATTR_ASRL_END_IN is set to
+     * VI_ASRL_END_TERMCHAR. This attribute is valid for both raw I/O (viRead) and formatted I/O (viScanf).
+     *
+     * @see
+     * <a href="https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_termchar_en.html">VI_ATTR_TERMCHAR_EN</a>
+     * @see <a href="https://github.com/pyvisa/pyvisa/blob/1.13.0/pyvisa/resources/messagebased.py#L96">read_termination
+     * in PyVISA</a>
+     * @see <a href="https://pyvisa.readthedocs.io/en/latest/introduction/communication.html">PyVISA docs page
+     * "Communicating with your instrument"</a>
+     */
+    public void setReadTerminationCharacterEnabled(boolean isReadTerminationCharacterEnabled) throws JVisaException {
+        setAttribute(JVisaLibrary.VI_ATTR_TERMCHAR_EN, isReadTerminationCharacterEnabled ? 1 : 0);
+    }
+
+    public boolean isReadTerminationCharacterEnabled() throws JVisaException {
+        return getAttributeBytes(JVisaLibrary.VI_ATTR_TERMCHAR_EN, 1)[0] == 1;
+    }
+
+    /**
+     * Specify a string which will be appended to all commands sent to the instrument, or null to not append a
+     * terminator.
+     *
+     * @see <a href="https://pyvisa.readthedocs.io/en/latest/introduction/communication.html">PyVISA docs page
+     * "Communicating with your instrument"</a>
+     */
+    public void setWriteTerminator(String writeTerminator) {
+        this.writeTerminator = writeTerminator;
+    }
+
+    public String getWriteTerminator() {
+        return writeTerminator;
     }
 
 }
