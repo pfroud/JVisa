690830d772ebed16343ded01f79aaed94a8a272d
Peter Froud <pfroud@gmail.com>
2023-08-03 16:53:28 -0700

    Add serial port example

---
+++ src/main/java/xyz/froud/jvisa_example/SerialPortExample.java
@@ -0,0 +1,27 @@
+package xyz.froud.jvisa_example;
+
+import xyz.froud.jvisa.JVisaException;
+import xyz.froud.jvisa.JVisaInstrument;
+import xyz.froud.jvisa.JVisaResourceManager;
+import xyz.froud.jvisa.SerialFlowControl;
+import xyz.froud.jvisa.SerialParity;
+import xyz.froud.jvisa.SerialStopBits;
+
+public class SerialPortExample {
+
+    public static void main(String[] args) throws JVisaException {
+        final String portName = "COM1";
+        try (JVisaResourceManager resourceManager = new JVisaResourceManager()) {
+            try (JVisaInstrument instrument = resourceManager.openInstrument(portName)) {
+                instrument.setSerialBaudRate(9600);
+                instrument.setSerialDataBits(8);
+                instrument.setSerialStopBits(SerialStopBits.ONE);
+                instrument.setSerialParity(SerialParity.NONE);
+                instrument.setSerialFlowControl(SerialFlowControl.NONE);
+                instrument.setReadTerminationCharacter('\n');
+                instrument.setWriteTerminator("\n");
+                System.out.println(instrument.queryString("*IDN?"));
+            }
+        }
+    }
+}
